fec6bb7c18b9bc8ddcb5ad4fcb047ee6303002ac
KhTalhaArshad <126275156+KhTalhaArshad@users.noreply.github.com>
2023-05-31 07:07:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
-pip install beautifulsoup4
+import stramlit as st
+import pandas as pd 
 import requests
 from bs4 import BeautifulSoup
 
-# Specify the URL of the website you want to scrape
-url = 'https://example.com'
+tag = st.selectbox('Choose a topic', ['love', 'humor', 'life', 'books'])
 
-# Send a GET request to the URL
-response = requests.get(url)
+generate = st.button('generate Csv')
 
-# Create a BeautifulSoup object to parse the HTML content
-soup = BeautifulSoup(response.text, 'html.parser')
+url = f"https://quotes.toscrape.com/tag/{tag}/"
 
-# Find specific elements on the page using CSS selectors
-# For example, let's extract all the links from the page
-links = soup.select('a')
+res = requests.get(url)
 
-# Loop through the extracted links and print their text and href attributes
-for link in links:
-    print('Text:', link.text)
-    print('URL:', link['href'])
-    print('---')
+content = Beautiful Soup(res.content, 'html.parser' )
+
+quotes = content.find_all('div', class_='quote')
+
+for quote in quotes: 
+   text = quote.find('span' , class_='text').text
+author = quote.find('small' , class_='author').text
+link = quote.find('a')
+st.success(text)
+st.markdown(f"<a href=https://quotes.toscrape.com{link['href']}>{author} </a>", unsafe_allow_html=True )
+quote_file.append([text, author, link['href'])
+
+if generate:
+     try: 
+            df = pd.DataFrame(quote_file)
+             df.to_csv('quotes.csv', index=False, header=['Quote', 'Author', 'Link'], encoding='cp1252')
+
+     except:      
+                   st.write('Loading...')
