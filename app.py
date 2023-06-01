cdf0c6e3ae20b8bec56c4f2735d54f5481742ddc
KhTalhaArshad <126275156+KhTalhaArshad@users.noreply.github.com>
2023-06-01 11:21:58 +0500

    app.py

--- app.py
+++ app.py
@@ -2,35 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 import streamlit as st
 
+# Define the URL to scrape
+url = "https://en.wikipedia.org/wiki/Neymar"
 
-def scrape_wikipedia(url):
-    # Send a GET request to the URL
-    response = requests.get(url)
+# Send a GET request to the URL
+response = requests.get(url)
 
-    if response.status_code == 200:
-        # Create a BeautifulSoup object
-        soup = BeautifulSoup(response.text, "html.parser")
+# Create a BeautifulSoup object to parse the HTML content
+soup = BeautifulSoup(response.content, "html.parser")
 
-        # Extract the desired information from the HTML
-        title = soup.title.string
+# Find the relevant data on the page
+title = soup.find("h1", id="firstHeading").text
+paragraphs = soup.find_all("p")
 
-        # Display the results using Streamlit
-        st.write(f"Title: {title}")
-    else:
-        st.write(f"Error: Unable to retrieve data from {url}")
+# Streamlit app
+st.title(title)
 
-
-def main():
-    # Define the URL to scrape
-    url = "https://www.wikipedia.org/"
-
-    # Set up Streamlit
-    st.title("Wikipedia Web Scraper")
-    st.write("Scraping data from Wikipedia...")
-
-    # Call the web scraping function
-    scrape_wikipedia(url)
-
-
-if __name__ == "__main__":
-    main()
+for paragraph in paragraphs:
+    st.write(paragraph.text)
