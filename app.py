a96d7cd9132cd9b9589bca819043cfcbfd893cf2
KhTalhaArshad <126275156+KhTalhaArshad@users.noreply.github.com>
2023-06-01 10:53:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,57 @@
-import urllib.request
+import requests
+
 from bs4 import BeautifulSoup
 
+# URL of the Wikipedia page for Asif Ali Zardari
 
-class Scraper:
-    def __init__(self, site):
-        self.site = site
+url = "https://en.m.wikipedia.org/wiki/Asif_Ali_Zardari"
 
-    def scrape(self):
-        r = urllib.request.urlopen(self.site)
-        html = r.read()
-        parser = "html.parser"
-        sp = BeautifulSoup(html,parser)
-        for tag in sp.find_all("a"):
-            url = tag.get("href")
-            if url is None:
-                continue
-            if "articles" in url:
-                print("\n" + url)
+# Send a GET request to the website
 
-news = "https://news.google.com/"
-Scraper(news).scrape()
+response = requests.get(url)
+
+# Check if the request was successful (status code 200)
+
+if response.status_code == 200:
+
+    # Parse the HTML content using BeautifulSoup
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Get the page title
+
+    title = soup.find("h1", {"id": "firstHeading"}).text
+
+    print("Page Title:", title)
+
+    # Find the infobox table on the page
+
+    infobox = soup.find("table", {"class": "infobox"})
+
+    # Find all the rows in the infobox
+
+    rows = infobox.find_all("tr")
+
+    # Extract the information from each row
+
+    print("Information:")
+
+    for row in rows:
+
+        header = row.find("th")
+
+        if header:
+
+            header_text = header.text.strip()
+
+            data_cell = row.find("td")
+
+            if data_cell:
+
+                data = data_cell.text.strip()
+
+                print(header_text + ":", data)
+
+else:
+
+    print("Failed to retrieve the website content.")
